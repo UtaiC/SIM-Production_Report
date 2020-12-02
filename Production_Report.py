657ac1c4b8b1ae49c71022450b815482eb2c9d4a
Utai <74049768+UtaiC@users.noreply.github.com>
2020-12-02 15:01:21 +0700

    New-Style

--- Production_Report.py
+++ Production_Report.py
@@ -27,8 +27,11 @@ MC=MC.fillna(0)
 QC=pd.read_excel('QC-Data-Nov-20.xlsx')
 QC=QC.fillna(0)
 ###################################
-st.subheader('Production DC report')
-DC
+
+if st.checkbox('Production DC report'):
+    st.subheader('Production DC report')
+    DC
+
 DC['Pct']=((DC['NG-Pcs'].sum()/DC['DC-Pcs'].sum())*100)
 st.subheader('DC Summarize')
 DCsum=DC[['DC-Pcs','BF-Pcs','NG-Pcs']].sum()
@@ -41,22 +44,26 @@ st.warning(DCpct)
 
 DCpph=DC['DC-Pcs']/(DC['Work-Hr']+DC['OT-Hr'])
 DCpph=DCpph.groupby('PartNo').mean()
-st.subheader('DC Performance Pcs/Hrs')
-st.table(DCpph)
-st.bar_chart(DCpph)
+
+if st.checkbox('DC Performance Pcs/Hrs'):
+    st.subheader('DC Performance Pcs/Hrs')
+    st.table(DCpph)
+    st.bar_chart(DCpph)
 st.subheader('DC SUM Performance Pcs/Hrs')
 DCpphm=DCpph.mean()
 st.success(DCpphm)
-
 DC['Pcsphr']=DC['DC-Pcs']/(DC['Work-Hr']+DC['OT-Hr'])
 DChdmc=DC[['HDMC','Pcsphr']].groupby('HDMC').mean()
-st.subheader('HDMC SUM Performance Pcs/Hrs')
-st.table(DChdmc)
-st.bar_chart(DChdmc)
+
+if st.checkbox('HDMC SUM Performance Pcs/Hrs'):
+    st.subheader('HDMC SUM Performance Pcs/Hrs')
+    st.bar_chart(DChdmc)
 
 #############################
-st.subheader('Production FN report')
-FN
+
+if st.checkbox('Production FN report'):
+    st.subheader('Production FN report')
+    FN
 FN['Pct']=(FN['NG-Pcs'].sum()/(FN['BM-Pcs']+FN['FG0-Pcs']).sum())*100
 st.subheader('FN Summarize')
 FNsum=FN[['BM-Pcs','FG0-Pcs','NG-Pcs']].sum()
@@ -67,15 +74,19 @@ st.warning(FNpct)
 
 FNpph=(FN['BM-Pcs']+FN['FG0-Pcs'])/(FN['Work-Hr']+FN['OT-Hr'])
 FNpph=FNpph.groupby('PartNo').mean()
-st.subheader('FN Performance Pcs/Hrs')
-st.table(FNpph)
-st.bar_chart(FNpph)
+
+if st.checkbox('FN Performance Pcs/Hrs'):
+    st.subheader('FN Performance Pcs/Hrs')
+    st.table(FNpph)
+    st.bar_chart(FNpph)
 st.subheader('FN SUM Performance Pcs/Hrs')
 FNpphm=FNpph.mean()
 st.success(FNpphm)
 #############################
-st.subheader('Production MC report')
-MC
+
+if st.checkbox('Production MC report'):
+    st.subheader('Production MC report')
+    MC
 MC['Pct']=(MC['NG-Pcs'].sum()/MC['MC-Pcs'].sum())*100
 st.subheader('MC Summarize')
 MCsum=MC[['MC-Pcs','M-FG0','NG-Pcs']].sum()
@@ -86,15 +97,20 @@ st.warning(MCpct)
 
 MCpph=MC['MC-Pcs']/(MC['Work-Hr']+MC['OT-Hr'])
 MCpph=MCpph.groupby('PartNo').mean()
-st.subheader('MC Performance Pcs/Hrs')
-st.table(MCpph)
-st.bar_chart(MCpph)
-st.subheader('MC SUM Performance Pcs/Hrs')
+
+if st.checkbox('MC Performance Pcs/Hrs'):
+    st.subheader('MC Performance Pcs/Hrs')
+    st.table(MCpph)
+    st.bar_chart(MCpph)
+
+
 MCpphm=MCpph.mean()
 st.success(MCpphm)
 #############################
-st.subheader('Production QC report')
-QC
+
+if st.checkbox('Production QC report'):
+    st.subheader('Production QC report')
+    QC
 
 QC['Pct']=(QC['TT-NG-Pcs'].sum()/QC['Sorted-Pcs'].sum())*100
 st.subheader('QC Summarize')
